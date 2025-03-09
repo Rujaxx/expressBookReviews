0184330369a7e38b8ad702d4a42bdf0eb94ecdd5
Rujaxx <shaikhrujan@gmail.com>
2025-03-09 07:11:37 -0400

    Final Tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,40 +20,52 @@ public_users.post("/register", (req,res) => {
   return res.status(201).json({message: "User added successfully",data:{username}});
 });
 
+const getAllBooks = async() => {
+    return Object.values(books)
+}
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json({message: "Books Fetched.",data:JSON.stringify(books)});
+public_users.get('/',async function (req, res) {
+    const allBooks = await getAllBooks()
+  return res.status(200).json({message: "Books Fetched.",data:JSON.stringify(allBooks)});
 })
 
+const getBook = async(isbn) => {
+    return books[isbn]  
+}
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
     const isbn = req.params.isbn
-    const book = books[isbn] ?? null
+    const book = await getBook(isbn) ?? null
     if(!book){
         return res.status(404).json({message:"Book not found."})
     }
     return res.status(200).json({message: "Books Fetched.",data:JSON.stringify(book)});
  });
   
-const getBookDetails = (field,value) => {
+const getBookDetails = async(field,value) => {
   const booksArr = Object.values(books)
   return booksArr.filter(book => book[field] == value)  
 }
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const bookList = getBookDetails("author",req.params.author)
+public_users.get('/author/:author',async function (req, res) {
+  const bookList = await getBookDetails("author",req.params.author)
   return res.status(200).json({message: "Books" , data : JSON.stringify(bookList)});
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const bookList = getBookDetails("title",req.params.title)
+public_users.get('/title/:title',async function (req, res) {
+    const bookList = await getBookDetails("title",req.params.title)
     return res.status(200).json({message: "Books" , data : JSON.stringify(bookList)});
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  return res.status(200).json({message:"Reviews",data:JSON.stringify(books[req.params.isbn].reviews)})
+public_users.get('/review/:isbn',async function (req, res) {
+    const reviews = await getBook(req.params.isbn)?.reviews
+    if(!reviews){
+        return res.status(404)._construct({message:"Book not found"})
+    }
+    return res.status(200).json({message:"Reviews",data:JSON.stringify(reviews)})
 });
 
 module.exports.general = public_users;
